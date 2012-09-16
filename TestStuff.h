0e1037e2357fbe8aedc60385c9d8290cee9f8b79
Brian Bell <brian.bell@theactigraph.com>
2012-09-16 00:22:58 -0500

    Adding a TestStuff class

---
+++ TestStuff.h
@@ -0,0 +1,13 @@
+//
+//  TestStuff.h
+//  TestApp
+//
+//  Created by Brian Bell on 9/16/12.
+//  Copyright (c) 2012 Brian Bell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestStuff : NSObject
+
+@end
---
+++ TestStuff.m
@@ -0,0 +1,13 @@
+//
+//  TestStuff.m
+//  TestApp
+//
+//  Created by Brian Bell on 9/16/12.
+//  Copyright (c) 2012 Brian Bell. All rights reserved.
+//
+
+#import "TestStuff.h"
+
+@implementation TestStuff
+
+@end
